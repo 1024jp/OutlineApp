9e07cfcbacd99b6c9aee953a80f69325d349d7ef
1024jp <1024jp@wolfrosch.com>
2024-10-27 14:41:52 +0900

    Initial Commit

---
+++ OutlineApp/Sources/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  OutlineApp
+//
+//  Created by 1024jp on 2024-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+// MARK: - Preview
+
+#Preview {
+    ContentView()
+}
---
+++ OutlineApp/Sources/OutlineAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  OutlineAppApp.swift
+//  OutlineApp
+//
+//  Created by 1024jp on 2024-10-27.
+//
+
+import SwiftUI
+
+@main
+struct OutlineAppApp: App {
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
